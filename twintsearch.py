e83c4bf2b8362e6b6d08036b9ab1ff2465b09f09
Richard Ferris <richard.ferris@gmail.com>
2022-11-27 15:28:14 +0000

    Update twintsearch.py
    Now writes to CSV more nicely, commas mess up the CSV structure so I put the tweet at the end.   You can then create a new row that concatenates the tweet back together.   Not idea but couldn't find a clean workaround.

--- twintsearch.py
+++ twintsearch.py
@@ -15,9 +15,9 @@ c.Store_csv = True
 c.User_full = True
 search = str(sys.argv[1])  #set the argument of the command to the search string (user or subject to scrape)
 c.Search = search
-c.Since = '2015-02-01 00:00:00'  #This gets ignored in this version of twint.
+c.Since = '2015-01-01 00:00:00'  #This gets ignored in this version of twint but can be compared against so I use it to stop the loop 
 
-c.Until = '2022-11-12 11:49:17'  #Date of the latest tweet to scrape, set it to current time to get everything
+c.Until = '2022-10-07 19:08:46'  #Date of the latest tweet to scrape, set it to current time to get everything
 c.Pandas = True
 
 prevuntil=c.Until
@@ -26,11 +26,23 @@ exceptioncount=0
 
 twint.run.Search(c)
 df = twint.storage.panda.Tweets_df
+flag=False
+csv_header=["id", "conversation_id", "created_at", "date", "timezone", "place", "language", "hashtags", "cashtags", "user_id", "user_id_str", "username", "name", "day", "hour", "link", "urls", "photos", "video", "thumbnail", "retweet", "nlikes", "nreplies", "nretweets", "quote_url", "search", "near", "geo", "source", "user_rt_id", "user_rt", "retweet_id", "reply_to", "retweet_date", "translate", "trans_src", "trans_dest", "tweet"]
 
-while(c.Until[0:4]!='2010'):      #This is a mess.   It stops the search when we reach 2014 but is done badly.  Will improve this.
+while(datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')>datetime.strptime(c.Since, '%Y-%m-%d %H:%M:%S')):      #   Stops the search when we reach c.Since
+    print("c.Until:" + str(datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')))
+    print("prevuntil :" + str(datetime.strptime(prevuntil, '%Y-%m-%d %H:%M:%S')))
+    if((datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')<datetime.strptime(prevuntil, '%Y-%m-%d %H:%M:%S'))):
+        print("c.Until:" + str(c.Until) + " was less than prevuntil:" + str(prevuntil))
+        #flag=True
+        print("set prevuntil to c.Until")
+        prevuntil=c.Until
     if((datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')>datetime.strptime(prevuntil, '%Y-%m-%d %H:%M:%S'))):
-        print("c.Until:"+str(c.Until)+" was greater than prevuntil:"+str(prevuntil))
+        print("c.Until:" + str(c.Until) + " was greater than prevuntil:" + str(prevuntil))
+        flag=True
+        print("set c.Until to prevuntil")
         c.Until=prevuntil
+    else:flag=False
 
     if(sleeptime>18):sleeptime=4    #found the backoff policy of 100 ws too lax so dropped it to 10
     try:
@@ -38,13 +50,19 @@ while(c.Until[0:4]!='2010'):      #This is a mess.   It stops the search when we
         df = twint.storage.panda.Tweets_df
         exceptioncount=0
     except:
-        print ("Exception while running search, blanking df")
+        print ("Exception while running search, blanking df")   
         df=''           #  If Twitter tells you to fuck of with your request for data, remember to blank the dataframe
 
     if(len(df)>1):
-        c.Until=(df.iloc[[-1]].iloc[0].date)    #Get the date of the last tweet and carry on searching from there
+        if (not flag):
+            c.Until=(df.iloc[[-1]].iloc[0].date)    #Get the date of the last tweet and carry on searching from there
         print ("Setting c.Until and prevuntil to : " + str(c.Until) + " from last tweet")
-        prevuntil=c.Until
+        if(datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')>datetime.strptime(prevuntil, '%Y-%m-%d %H:%M:%S')):
+            print("c.Until time of " + str(c.Until) + " greater than prevuntil of : " + str(prevuntil) + "Putting pack to prevuntil and taking off 10 minutes")
+            c.Until=prevuntil
+            c.Until=datetime.strftime(datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')+timedelta(minutes=-10), '%Y-%m-%d %H:%M:%S')
+        print("writing dataframe to " + str(sys.argv[1]) + ".csv")
+        df.to_csv((sys.argv[1]+".csv"), mode='a', index=False, header=False, columns=csv_header)
         if(sleeptime>5):sleeptime-=1      #  Staggered backoff policy
     else:
         exceptioncount=exceptioncount+1
@@ -53,7 +71,8 @@ while(c.Until[0:4]!='2010'):      #This is a mess.   It stops the search when we
         c.Until=prevuntil
         time.sleep (sleeptime)
         sleeptime+=1
-        #if(exceptioncount>1):
-        #    print("more than one excpetion, taking an hour from c.Until")
-        #    c.Until=datetime.strftime(datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')-timedelta(hours=1),'%Y-%m-%d %H:%M:%S')
+        #if(exceptioncount>1):      #Uncomment this bit if your search gets "stuck" and won't get past a certain point, possibly due to twitter's bot detection / there being no tweets.
+        #    print("more than one excpetion, taking an hour from c.Until")   
+        #    c.Until=datetime.strftime(datetime.strptime(c.Until, '%Y-%m-%d %H:%M:%S')-timedelta(hours=1),'%Y-%m-%d %H:%M:%S')  #if still no joy, copy this line to the very start of the loop, changing hours=1 to minutes=15
         #    print("c.Until = "+str(c.Until))
+
